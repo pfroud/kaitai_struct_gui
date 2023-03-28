538735de1e2f6a0f3f25387918ab48c78fd5f6a1
Peter Froud <pfroud@erp-power.com>
2023-03-27 21:31:57 -0700

    Select tree nodes when bytes are selected

--- src/main/java/io/kaitai/struct/visualizer/KaitaiStructVisualizerPanel.java
+++ src/main/java/io/kaitai/struct/visualizer/KaitaiStructVisualizerPanel.java
@@ -17,13 +17,26 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Font;
 import java.lang.reflect.Method;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import javax.swing.tree.TreeNode;
 import ru.mingun.kaitai.struct.Span;
 import ru.mingun.kaitai.struct.tree.KaitaiStructTreeModel;
+import tv.porst.jhexview.SelectionModel.Interval;
 
 public class KaitaiStructVisualizerPanel extends JPanel {
 
     private final JTree JTREE;
     private final JSplitPane SPLIT_PANE;
+
+    /**
+     * This field is to prevent repeated selection events from happening. The JTree selection
+     * listener changes the hex viewer selection, and the hex viewer selection listener changes the
+     * JTree selection.
+     */
+    private boolean runEventListeners = true;
 
     public KaitaiStructVisualizerPanel(KaitaiStruct kaitaiStruct) throws ReflectiveOperationException {
         super();
@@ -34,6 +47,7 @@ public class KaitaiStructVisualizerPanel extends JPanel {
         hexViewer.setBytesPerColumn(1);
         hexViewer.setColumnSpacing(8);
         hexViewer.setHeaderFontStyle(Font.BOLD);
+        hexViewer.setEditable(false);
 
         hexViewer.setFontColorHeader(new Color(0x0000C0));
         hexViewer.setFontColorOffsetView(new Color(0x0000C0));
@@ -53,6 +67,10 @@ public class KaitaiStructVisualizerPanel extends JPanel {
         JTREE.setShowsRootHandles(true);
 
         JTREE.addTreeSelectionListener(event -> {
+            if (!runEventListeners) {
+                return;
+            }
+            runEventListeners = false;
             hexViewer.getSelectionModel().clearSelection();
             if (JTREE.getSelectionPaths() == null) {
                 return;
@@ -70,6 +88,7 @@ public class KaitaiStructVisualizerPanel extends JPanel {
                     }
                 }
             }
+            runEventListeners = true;
         });
 
         // https://docs.oracle.com/javase/tutorial/uiswing/components/tree.html#display
@@ -96,13 +115,145 @@ public class KaitaiStructVisualizerPanel extends JPanel {
         final Method readMethod = kaitaiStruct.getClass().getDeclaredMethod("_read");
         readMethod.setAccessible(true);
         readMethod.invoke(kaitaiStruct);
-        JTREE.setModel(new KaitaiStructTreeModel(kaitaiStruct));
+        final KaitaiStructTreeModel treeModel = new KaitaiStructTreeModel(kaitaiStruct);
+        JTREE.setModel(treeModel);
 
         ///////////////////////// Set hex viewer data ///////////////////////////////
         kaitaiStruct._io().seek(0);
         final byte[] allBytes = kaitaiStruct._io().readBytesFull();
         hexViewer.setData(new SimpleDataProvider(allBytes));
         hexViewer.setDefinitionStatus(JHexView.DefinitionStatus.DEFINED);
+
+        /////////////////////// Populate map for the hex viewer selection listener////////////////
+        /*
+        This map is used to select tree nodes from a hex viewer selection.
+        We never do a lookup on this map. It is just used as a Set of pairs.
+         */
+        final Map<TreePath, Span> mapTreePathToSpan = new HashMap<>();
+        populateMapOfTreePathToSpan(mapTreePathToSpan, treeModel.getRoot(), new TreePath(treeModel.getRoot()));
+
+        /////////////////////// Add hex viewer selection listener ///////////////
+        hexViewer.getSelectionModel().addSelectionListener(selectionEvent -> {
+            if (!runEventListeners) {
+                return;
+            }
+
+            runEventListeners = false;
+
+            JTREE.getSelectionModel().clearSelection();
+
+            final Iterator<Interval> selectedIntervals = hexViewer.getSelectionModel().iterator();
+            while (selectedIntervals.hasNext()) {
+                final Interval selectedIntervalNibbles = selectedIntervals.next();
+
+                /*
+                The hex viewer selection interval is in nibbles but Kaitai Struct
+                uses bytes.
+                 */
+                final int selectionStart = (int) Math.floor(selectedIntervalNibbles.getStart() / 2.0);
+                final int selectionEnd = (int) Math.ceil(selectedIntervalNibbles.getEnd() / 2.0);
+
+                for (Map.Entry<TreePath, Span> entry : mapTreePathToSpan.entrySet()) {
+                    final TreePath treePath = entry.getKey();
+                    final Span span = entry.getValue();
+                    final long spanStart = span.getStart();
+                    final long spanEnd = span.getEnd();
+
+                    final boolean selectTheTreeNode;
+                    /*
+                     Check if the left edge of the selection is before the span.
+                     */
+                    if (selectionStart < spanStart) {
+                        /*
+                        The left edge of the selection is before the span.
+
+                        Select the tree node only if the right edge of the selection is either
+                        inside the span or goes past the right edge of the span.
+                         */
+                        if (selectionEnd > spanStart) {
+                            /*
+                            The right edge of the selection is inside the span or goes past the
+                            right edge of the span.
+
+                            Example:
+
+                            selection   v-------v
+                               bytes  0 1 2 3 4 5 6 7 8 9
+                                span          ^---^
+
+                            or
+
+                            selection    v-------------v
+                                bytes  0 1 2 3 4 5 6 7 8 9
+                                 span          ^---^
+                             */
+                            selectTheTreeNode = true;
+                        } else {
+                            /*
+                            The right edge of the span is to the left of the span.
+
+                            Example:
+
+                            selection    v---v
+                                bytes  0 1 2 3 4 5 6 7 8 9
+                                 span          ^---^
+                             */
+                            selectTheTreeNode = false;
+                        }
+                    } else {
+                        /*
+                        The left edge of the selection is either inside the span
+                        or past the right edge of the span.
+
+                        Select the tree node only if the left edge of the selection is inside
+                        the span.
+                         */
+                        if (selectionStart < spanEnd) {
+                            /*
+                            The left edge of the selection is inside the span.
+
+                            Example:
+
+                            selection         v-----v
+                               bytes  0 1 2 3 4 5 6 7 8 9
+                                span      ^-----^
+                             */
+                            selectTheTreeNode = true;
+                        } else {
+                            /*
+                            The left edge of the selection is to the right of the span.
+
+                            Example:
+
+                            selection                v---v
+                                bytes  0 1 2 3 4 5 6 7 8 9
+                                 span      ^-----^
+                             */
+                            selectTheTreeNode = false;
+                        }
+                    }
+
+                    if (selectTheTreeNode) {
+                        JTREE.getSelectionModel().addSelectionPath(treePath);
+                    }
+                }
+            }
+            runEventListeners = true;
+        });
+    }
+
+    private void populateMapOfTreePathToSpan(Map<TreePath, Span> destination, ChunkNode root, TreePath thePathSoFar) {
+        if (root.isLeaf()) {
+            destination.put(thePathSoFar, root.getSpan());
+        } else {
+            final Enumeration<? extends TreeNode> children = root.children();
+            while (children.hasMoreElements()) {
+                final TreeNode child = children.nextElement();
+                if (child instanceof ChunkNode) {
+                    populateMapOfTreePathToSpan(destination, (ChunkNode) child, thePathSoFar.pathByAddingChild(child));
+                }
+            }
+        }
     }
 
     public void setSplitPaneDividerLocation(int location) {
